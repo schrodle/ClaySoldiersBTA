7722ef0d1e511bb5a2539bc3386d17e958e03015
Neckbeard-Tim <60363811+Neckbeard-Tim@users.noreply.github.com>
2024-09-12 14:53:45 -0500

    Update ClaySoldiers.java
    * Replaced deprecated methods

--- src/main/java/rasvhw/claysoldiers/ClaySoldiers.java
+++ src/main/java/rasvhw/claysoldiers/ClaySoldiers.java
@@ -1,8 +1,11 @@
 package rasvhw.claysoldiers;
 
+import  java.util.function.Supplier;
+
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.LivingRenderer;
+import net.minecraft.client.render.item.model.ItemModelStandard;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.item.Item;
@@ -20,6 +23,7 @@ import rasvhw.claysoldiers.model.RenderClayMan;
 import rasvhw.claysoldiers.model.RenderDirtHorse;
 import rasvhw.claysoldiers.recipes.ClaySoldiersRecipes;
 import turniplabs.halplibe.helper.EntityHelper;
+import turniplabs.halplibe.helper.ItemBuilder;
 import turniplabs.halplibe.helper.ItemHelper;
 import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
@@ -43,6 +47,105 @@ public class ClaySoldiers implements ModInitializer, GameStartEntrypoint, Recipe
 	/*
 		0-Black, 1-Red, 2-Green, 3-Brown, 4-Blue, 5-Purple, 6-Cyan, 7-Light Gray, 8-Gray, 9-Pink, 10-Lime, 11-Yellow, 12-Light Blue, 13-Magenta, 14-Orange, 15-White
 	*/
+	public static final Item greyDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_grey")
+		.build(new ItemClayMan("greySoldier", getNextId(), -1));
+
+	public static final Item blackDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_black")
+		.build(new ItemClayMan("blackSoldier", getNextId(), 0));  // Black
+
+	public static final Item redDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_red")
+		.build(new ItemClayMan("redSoldier", getNextId(), 1));  // Red
+
+	public static final Item greenDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_green")
+		.build(new ItemClayMan("greenSoldier", getNextId(), 2));  // Green
+
+	public static final Item brownDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_brown")
+		.build(new ItemClayMan("brownSoldier", getNextId(), 3));  // Brown
+
+	public static final Item blueDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_blue")
+		.build(new ItemClayMan("blueSoldier", getNextId(), 4));  // Blue
+
+	public static final Item purpleDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_purple")
+		.build(new ItemClayMan("purpleSoldier", getNextId(), 5));  // Purple
+
+	public static final Item cyanDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_cyan")
+		.build(new ItemClayMan("cyanSoldier", getNextId(), 6));  // Cyan
+
+	public static final Item lightGrayDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_lightGray")
+		.build(new ItemClayMan("lightGraySoldier", getNextId(), 7));  // Light Gray
+
+	public static final Item grayDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_gray")
+		.build(new ItemClayMan("graySoldier", getNextId(), 8));  // Gray
+
+	public static final Item pinkDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_pink")
+		.build(new ItemClayMan("pinkSoldier", getNextId(), 9));  // Pink
+
+	public static final Item limeDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_lime")
+		.build(new ItemClayMan("limeSoldier", getNextId(), 10));  // Lime
+
+	public static final Item yellowDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_yellow")
+		.build(new ItemClayMan("yellowSoldier", getNextId(), 11));  // Yellow
+
+	public static final Item lightBlueDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_lightBlue")
+		.build(new ItemClayMan("lightBlueSoldier", getNextId(), 12));  // Light Blue
+
+	public static final Item magentaDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_magenta")
+		.build(new ItemClayMan("magentaSoldier", getNextId(), 13));  // Magenta
+
+	public static final Item orangeDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_orange")
+		.build(new ItemClayMan("orangeSoldier", getNextId(), 14));  // Orange
+
+	public static final Item whiteDoll = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_white")
+		.build(new ItemClayMan("whiteSoldier", getNextId(), 15));  // White
+
+	//other
+	public static final Item dirtHorse = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/doll_horse")
+		.build(new ItemDirtHorse("dirtHorse", getNextId()));
+	public static final Item clayDisruptor = new ItemBuilder(MOD_ID)
+		.setItemModel(item -> new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D())
+		.setIcon(MOD_ID + ":item/disruptor")
+		.build(new ItemClayDisruptor("clayDisruptor",getNextId()));
+
+	//public static final Item dirtHorse = ItemHelper.createItem(MOD_ID, new ItemDirtHorse("dirtHorse",getNextId()));
+	//public static final Item clayDisruptor = ItemHelper.createItem(MOD_ID, );
+
+	/*
 	public static final Item greyDoll = ItemHelper.createItem(MOD_ID, new ItemClayMan("greySoldier", getNextId(), -1),"doll_grey.png");
 	public static final Item blackDoll=ItemHelper.createItem(MOD_ID,new ItemClayMan("blackSoldier",getNextId(),0),"doll_black.png");
 	public static final Item redDoll = ItemHelper.createItem(MOD_ID, new ItemClayMan("redSoldier",getNextId(), 1), "doll_red.png");
@@ -63,14 +166,16 @@ public class ClaySoldiers implements ModInitializer, GameStartEntrypoint, Recipe
 	//Other
 	public static final Item dirtHorse = ItemHelper.createItem(MOD_ID, new ItemDirtHorse("dirtHorse",getNextId()), "doll_horse.png");
 	public static final Item clayDisruptor = ItemHelper.createItem(MOD_ID, new ItemClayDisruptor("clayDisruptor",getNextId()), "disruptor.png");
-
+	*/
 	@Override
 	public void onInitialize() {
-		EntityHelper.Core.createEntity(EntityClayMan.class, 200,"ClaySoldier");
-		EntityHelper.Client.assignEntityRenderer(EntityClayMan.class, new RenderClayMan(new ModelClayMan(0.0F,13.0F),.125F));
+		Supplier<EntityRenderer<?>> clayManSupplier = () -> new RenderClayMan(new ModelClayMan(0.0F,13.0F),.125F);
+		EntityHelper.createEntity(EntityClayMan.class, 200,"ClaySoldier", clayManSupplier);
+		//EntityHelper.Assignment.queueEntityRenderer(EntityClayMan.class, new RenderClayMan(new ModelClayMan(0.0F,13.0F),.125F));
 		//EntityHelper.createEntity(EntityClayMan.class, new RenderClayMan(new ModelClayMan(0.0F, 13.0F), 0.125F), 200, "ClaySoldier");
-		EntityHelper.Core.createEntity(EntityDirtHorse.class,201,"DirtHorse");
-		EntityHelper.Client.assignEntityRenderer(EntityDirtHorse.class, new RenderDirtHorse(new ModelDirtHorse(0.0F,12.75F),.15F));
+		Supplier<EntityRenderer<?>> dirtHorseSupplier = () -> new RenderDirtHorse(new ModelDirtHorse(0.0F, 12.75F), 0.15F);
+		EntityHelper.createEntity(EntityDirtHorse.class,201,"DirtHorse", dirtHorseSupplier);
+		//EntityHelper.Assignment.queueEntityRenderer(EntityDirtHorse.class, new RenderDirtHorse(new ModelDirtHorse(0.0F,12.75F),.15F));
 		//EntityHelper.createEntity(EntityDirtHorse.class, new RenderDirtHorse(new ModelDirtHorse(0.0F, 12.75F), 0.15F), 201, "DirtHorse");
 
 		LOGGER.info("Initialized!");
@@ -97,5 +202,10 @@ public class ClaySoldiers implements ModInitializer, GameStartEntrypoint, Recipe
 	@Override
 	public void onRecipesReady() {
 		new ClaySoldiersRecipes().initializeRecipes();
+	}
+
+	@Override
+	public void initNamespaces() {
+		//unused
 	}
 }
